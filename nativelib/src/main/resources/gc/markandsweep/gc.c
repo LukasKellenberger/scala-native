bcc8d18ce6522e5b71e2024ce9e71c14b846783f
Lukas Kellenberger <lukas.kellenberger@gmail.com>
2017-03-07 15:11:54 +0100

    Fix roots with -O2

--- nativelib/src/main/resources/gc/markandsweep/gc.c
+++ nativelib/src/main/resources/gc/markandsweep/gc.c
@@ -25,8 +25,11 @@ void mark(word_t* block);
 word_t* heap_start = NULL;
 word_t* heap_end = NULL;
 
+Bitmap* bitmap_copy = NULL;
+
 long count = 0;
 long count2 = 0;
+long count3 = 0;
 
 extern word_t** __MODULES__;
 extern int __MODULES_SIZE__;
@@ -36,22 +39,66 @@ inline static int in_heap(word_t* block) {
 }
 
 void memory_check();
+void mark(word_t* block);
+
+void mark_inner(word_t* inner_ptr) {
+    word_t* current = inner_ptr - 1;
+    while(!bitmap_get_bit(bitmap_copy, current)) {
+        current -= 1;
+    }
+    size_t size = header_unpack_size(current);
+    if(current + size <= inner_ptr) {
+        mark(current);
+    }
+}
 
 void _mark(word_t* block) {
+    assert(bitmap_get_bit(free_list->bitmap, block));
+    assert(header_unpack_tag(block) == tag_allocated);
+    assert(in_heap(block));
+
     bitmap_clear_bit(free_list->bitmap, block);
 
-    /*Rtti* rtti = get_rtti(block);
-    printf("##### rtti size %ld\n", rtti->size);*/
-    size_t size = header_unpack_size(block);
 
-    for (int i = 0; i < size; i++) {
-        word_t field = block[i + 1];
-        word_t* field_addr = (word_t*)field-1;
+    Rtti rtti = *((Rtti*) *(block+1));
+    //printf("%p\n", block);
+    //printf("#### id: %d\n", rtti.id);
+    /*printf("##### rtti size %lld\n", rtti.size);
+    printf("### offset: %p\n", rtti.ptr_map);
+    fflush(stdout);*/
+
+    size_t size = header_unpack_size(block);
+    assert(size < heap_end - heap_start);
+
+    /*int64_t* ptr_map = rtti.ptr_map;
+    int i=0;
+    while(ptr_map[i] != -1) {
+        assert(ptr_map[i] % 8 == 0);
+        word_t field = block[ptr_map[i]/sizeof(word_t) + 1];
+        word_t* field_addr = (word_t*)field - 1;
         if(in_heap(field_addr)) {
             if (bitmap_get_bit(free_list->bitmap, field_addr)) {
                 mark(field_addr);
             } else {
                 count2++;
+            }
+        }
+        ++i;
+    }*/
+
+
+
+    for (int i = 0; i < size - 1; i++) {
+        word_t field = block[i + 2];
+        word_t* field_addr = (word_t*)field-1;
+        if(in_heap(field_addr)) {
+            if (bitmap_get_bit(free_list->bitmap, field_addr)) {
+                mark(field_addr);
+            } else if(!bitmap_get_bit(bitmap_copy, block)) {
+                count2++;
+                mark_inner(field_addr);
+            } else {
+                count3++;
             }
         }
     }
@@ -60,28 +107,99 @@ void _mark(word_t* block) {
  * Mark (pointer to header)
  */
 void mark(word_t* block) {
-    count++;
     // Check if pointer is on block header
 
     tag_t tag = header_unpack_tag(block);
     if (bitmap_get_bit(free_list->bitmap, block) && tag == tag_allocated) {
+        count++;
         _mark(block);
-    } else {
+    } else if(!bitmap_get_bit(bitmap_copy, block)) {
+        printf("inner ptr\n");
         count2++;
-        /*word_t* start = free_list->list->start
-        //printf("could be inner pointer\n");
-        word_t* current = block;
-        while(current >= start && !bitmap_get_bit(bitmap_copy, addr_p_to_v(current))) {
-            current -= 1;
+        mark_inner(block);
+    } else {
+        count3++;
+    }
+}
+
+void mark_roots_registers(unw_cursor_t* cursor) {
+    unw_word_t reg;
+
+    int regs[] = {UNW_X86_64_RAX,
+                  UNW_X86_64_RDX,
+                  UNW_X86_64_RCX,
+                  UNW_X86_64_RBX,
+                  UNW_X86_64_RSI,
+                  UNW_X86_64_RDI,
+                  UNW_X86_64_RBP,
+                  UNW_X86_64_RSP,
+                  UNW_X86_64_R8,
+                  UNW_X86_64_R9,
+                  UNW_X86_64_R10,
+                  UNW_X86_64_R11,
+                  UNW_X86_64_R12,
+                  UNW_X86_64_R13,
+                  UNW_X86_64_R14,
+                  UNW_X86_64_R15};
+
+    for(int i=0; i < 17; i++) {
+        unw_get_reg(cursor, regs[i], &reg);
+        word_t* pp = (word_t*)reg - 1;
+        if(in_heap(pp)) {
+            mark(pp);
+        }
+    }
+}
+
+void mark_roots_stack(unw_cursor_t* cursor) {
+    unw_word_t top = 0, bottom = 0;//0x7fff5fc00000;
+    while (unw_step(cursor) > 0) {
+        unw_word_t rbp, rsp, offset;
+
+        unw_get_reg(cursor, UNW_X86_64_RBP, &rbp);
+        unw_get_reg(cursor, UNW_X86_64_RSP, &rsp);
+        /*printf("rsp: %p\n", rsp);
+        printf("rbp: %p\n", rbp);
+        printf("top: %p\n", top);
+        printf("bottom: %p\n", bottom);*/
+
+        char sym[256];
+        //unw_get_proc_name(cursor, sym, sizeof(sym), &offset);
+        //printf(" (%s+0x%lx)\n", sym, offset);
+        //fflush(stdout);
+
+        if(top == 0 || rsp < top) {
+            top = rsp;
+        }
+        if(bottom == 0 || rbp > bottom) {
+            bottom = rbp;
         }
 
-        if(current >= start) {
+        if(rsp > bottom) {
+            bottom = rsp;
+        }
+    }
+    unw_word_t p = top;
 
-            tag_t tag = header_unpack_tag(*current);
-            if(bitmap_get_bit(bitmap, addr_p_to_v(current)) && tag == tag_Allocated_Block) {
-                _mark(current, addr_p_to_v(current));
-            }
-        }*/
+    while(p < bottom) {
+
+        word_t* pp = (*(word_t**)p) - 1;
+        if(in_heap(pp)) {
+            mark(pp);
+        }
+        p += 8;
+    }
+}
+
+void mark_roots_modules() {
+    word_t** module = __MODULES__;
+    int nb_modules = __MODULES_SIZE__;
+
+    for(int i=0; i < nb_modules; i++) {
+        word_t* addr = module[i] - 1;
+        if(in_heap(addr)) {
+            mark(addr);
+        }
     }
 }
 
@@ -89,40 +207,14 @@ void mark_roots() {
     unw_cursor_t cursor;
     unw_context_t context;
 
-    setjmp;
+    //setjmp;
     // Initialize cursor to current frame for local unwinding.
     unw_getcontext(&context);
     unw_init_local(&cursor, &context);
-    // Unwind frames one by one, going up the frame stack.
-    while (unw_step(&cursor) > 0) {
-        unw_word_t rbp, rsp;
 
-        unw_get_reg(&cursor, UNW_X86_64_RBP, &rbp);
-        unw_get_reg(&cursor, UNW_X86_64_RSP, &rsp);
-
-        unw_word_t p = rsp;
-        while(p < rbp) {
-            word_t* pp = *(word_t**)p;
-            if(in_heap(pp - 1)) {
-                mark(pp - 1);
-                p += 4;
-            } else {
-                p += 4;
-            }
-        }
-    }
-
-    word_t** module = __MODULES__;
-    int nb_modules = __MODULES_SIZE__;
-
-    for(int i=0; i < nb_modules; i++) {
-        word_t* addr = module[i] - 1;
-        if(in_heap(addr)) {
-            //printf("mark module %p\n", addr);
-            //fflush(stdout);
-            mark(addr);
-        }
-    }
+    mark_roots_registers(&cursor);
+    mark_roots_stack(&cursor);
+    mark_roots_modules();
 }
 
 void sweep() {
@@ -170,11 +262,14 @@ void scalanative_init() {
     LinkedList* linkedList = free_list->list;
     heap_start = linkedList->start;
     heap_end = heap_start + (linkedList->size / sizeof(word_t));
+
+    Bitmap* bitmap = free_list->bitmap;
+    bitmap_copy = bitmap_alloc(bitmap->size, bitmap->offset);
 }
 
 
 void* scalanative_alloc_raw(size_t size) {
-    size = ((size + sizeof(word_t) - 1 ) / sizeof(word_t)) * sizeof(word_t);
+    size = (size + 16 - 1 ) / 16 * 16;
     if(free_list == NULL) {
         scalanative_init();
     }
@@ -223,13 +318,17 @@ void* alloc(size_t size) {
 
 void scalanative_collect() {
     memory_check();
+    bitmap_clone(free_list->bitmap, bitmap_copy);
     count = 0;
     count2 = 0;
-    //printf("\n\n### START GC ###\n");
+    count3 = 0;
+    printf("\n\n### START GC ###\n");
+
+
     //free_list_print_stats(free_list);
     //free_list_print(free_list);
     //print_memory(free_list);
-    //fflush(stdout);
+    fflush(stdout);
 
     mark_roots();
     //free_list_print(free_list);
@@ -238,11 +337,12 @@ void scalanative_collect() {
     sweep();
     //free_list_print(free_list);
     //free_list_print_stats(free_list);
-    //printf("count: %ld\n", count);
-    //printf("inner ptrs: %ld\n", count2);
+    printf("count: %ld\n", count);
+    printf("inner ptrs: %ld\n", count2);
+    printf("else ptrs: %ld\n", count3);
 
-    //printf("### END GC ###\n");
-    //fflush(stdout);
+    printf("### END GC ###\n");
+    fflush(stdout);
     //memory_check();
 
 
