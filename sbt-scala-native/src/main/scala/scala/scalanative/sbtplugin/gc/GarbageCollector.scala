4604413773ed121d675a50f7b6942b7b30c9f1a7
Lukas Kellenberger <lukas.kellenberger@gmail.com>
2017-03-13 18:15:27 +0100

    Fix build

--- sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/gc/GarbageCollector.scala
+++ sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/gc/GarbageCollector.scala
@@ -9,16 +9,26 @@ object GarbageCollector {
   val dir = "gc"
 }
 
-case class GarbageCollector(name: String, links: Seq[String]) {
+case class GarbageCollector(name: String, links: Seq[String] = Nil) {
   private val specificDir = s"$dir/$name"
 
-  def isOtherGC(path: String): Boolean =
-    path.contains(dir) && !path.contains(specificDir)
+  /**
+   *
+   * Used to find files that should not be compiled
+   *
+   * @param path the path to the file
+   * @param nativelib nativelib directory
+   * @return true if path is in the nativelib gc directory but not the current GC, false otherwise
+   */
+  def isOtherGC(path: String, nativelib: File): Boolean = {
+    val nativeGCPath = nativelib.toPath.resolve(dir)
+    path.contains(nativeGCPath.toString) && !path.contains(specificDir)
+  }
 
-  def filterFiles(files: Seq[File]): Seq[File] =
-    files.filterNot(f => isOtherGC(f.getPath().toString))
+  def filterFiles(files: Seq[File], nativelib: File): Seq[File] =
+    files.filterNot(f => isOtherGC(f.getPath().toString, nativelib))
 
 }
 
-final object NoGC extends GarbageCollector("nogc", Seq())
+final object NoGC extends GarbageCollector("nogc")
 final object BoehmGC extends GarbageCollector("boehm", Seq("gc"))
