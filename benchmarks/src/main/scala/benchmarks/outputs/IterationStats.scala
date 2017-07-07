015b6b12712fbc8adde30b4c4d20680658980bf2
Lukas Kellenberger <lukas.kellenberger@gmail.com>
2017-07-07 16:22:07 +0200

    format

--- benchmarks/src/main/scala/benchmarks/outputs/IterationStats.scala
+++ benchmarks/src/main/scala/benchmarks/outputs/IterationStats.scala
@@ -2,23 +2,24 @@ package benchmarks.outputs
 
 import benchmarks.{BenchmarkOutput, MultirunResult, MultirunSuccess}
 
-
 object IterationStats extends BenchmarkOutput {
-  override def generate(results: Seq[MultirunResult], args: List[String]): Unit = {
-    val successes = results collect { case result @ MultirunSuccess(_, _, _, iters) if iters.isDefined => result }
-    successes.foreach {
-      success =>
-        val iterations = success.iterationsPerRun.get
-        val runs = success.nbRuns
-        val (avg, std) = (0 until iterations).map {
-          i =>
-            val values = (0 until runs).map(r => success.times(i + r * iterations))
-            val avg = average(values)
-            (avg, standardDeviation(values, avg))
-        }.unzip
+  override def generate(results: Seq[MultirunResult],
+                        args: List[String]): Unit = {
+    val successes = results collect {
+      case result @ MultirunSuccess(_, _, _, iters) if iters.isDefined =>
+        result
+    }
+    successes.foreach { success =>
+      val iterations = success.iterationsPerRun.get
+      val runs       = success.nbRuns
+      val (avg, std) = (0 until iterations).map { i =>
+        val values = (0 until runs).map(r => success.times(i + r * iterations))
+        val avg    = average(values)
+        (avg, standardDeviation(values, avg))
+      }.unzip
 
-        println(s"${success.name},average,${avg.mkString(",")}")
-        println(s"${success.name},stddev,${std.mkString(",")}")
+      println(s"${success.name},average,${avg.mkString(",")}")
+      println(s"${success.name},stddev,${std.mkString(",")}")
     }
   }
 
